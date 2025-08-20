d6a6a3ad312432aef490c57c6cd5aa51e835fc7b
Bruno H. Homem <brunohhomem@gmail.com>
2025-08-20 08:58:22 -0300

    feat: implements jwtAuthGuard

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,8 +2,8 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CreateUserDto } from './dto/create-user-dto';
 import { HashingService } from 'src/common/hashing/hashing.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
